311c2c7b14680869717e17d1871cca0f40463283
GlebShulga <shulga_gleb@hotmail.com>
2021-09-02 12:10:42 +1000

    add checkbox to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "./Header.scss";
 
 const Header = ({ task }) => {
+
   // const onClickEditTask = () => {
   //   setEditingTaskMode(true);
   //   setChoosenTask(task);
@@ -12,17 +13,28 @@ const Header = ({ task }) => {
   return (
     <div className="Header">
       <div className="Header-title">To-Do List</div>
-      <form action="" className="Header-form">
-        <input
-          type="text"
-          placeholder="Search"
-          name="search"
-          className="Header-form_input"
-        />
-        <button className="Header-form_button" type="submit">
-          <FontAwesomeIcon icon={faSearch} />
-        </button>
-      </form>
+      <div className="Header-filter-form">
+        <div className="EditTask-status">
+          <input
+            type="checkbox"
+            id="statusDone"
+            name="statusDone"
+            value={true}
+          />
+          <label htmlFor="statusDone">Show done</label>
+        </div>
+        <form action="" className="Header-form">
+          <input
+            type="text"
+            placeholder="Search"
+            name="search"
+            className="Header-form_input"
+          />
+          <button className="Header-form_button" type="submit">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
